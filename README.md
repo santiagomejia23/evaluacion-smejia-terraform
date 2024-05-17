7342b1b1a13a5e004cc59d108c5a3ab4576085d8
Santiago <santiago.mejia@f2x.com.co>
2024-05-17 15:58:25 -0500

    se crea la infraestructura

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # evaluacion-smejia-terraform
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 5.50.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_cloudwatch_log_group.my_log_group](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group) | resource |
+| [aws_cloudwatch_log_stream.my_log_stream](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_stream) | resource |
+| [aws_iam_instance_profile.ec2_instance_profile](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile) | resource |
+| [aws_iam_role.ec2_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy.ec2_role_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_instance.terraform-example](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance) | resource |
+| [aws_internet_gateway.my_igw](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway) | resource |
+| [aws_key_pair.my_key_pair](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair) | resource |
+| [aws_route_table.route_table](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table) | resource |
+| [aws_route_table_association.my_subnet_association](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association) | resource |
+| [aws_s3_bucket.sm_bucket](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket) | resource |
+| [aws_s3_bucket_object.outputs_folder](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object) | resource |
+| [aws_security_group.my_security_group](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group) | resource |
+| [aws_subnet.subnet1](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet) | resource |
+| [aws_vpc.my_vpc](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc) | resource |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ maint.tf
@@ -0,0 +1,219 @@
+terraform {
+  backend "s3" {
+    bucket = "sm-bucket-evaluacion"
+    key    = "states/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "test"
+}
+
+resource "aws_s3_bucket" "sm_bucket" {
+  bucket = "sm-bucket-evaluacion"
+
+  lifecycle_rule {
+    id      = "log"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_s3_bucket_object" "outputs_folder" {
+  bucket = aws_s3_bucket.sm_bucket.bucket
+  key    = "outputs/"
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_iam_role_policy" "ec2_role_policy" {
+  name = "ec2_role_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::sm-bucket-evaluacion/*"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "key-sm"
+  public_key = file("/home/santiago/.ssh/id_rsa.pub")
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name     = "vpc_evalua_SM",
+    username = "smejia"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name     = "subnet_evalu_SM1",
+    username = "smejia"
+  }
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "mi-grupo-de-seguridad"
+  description = "Reglas de seguridad para la instancia EC2"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_instance" "terraform-example" {
+  ami           = "ami-012485deee5681dc0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet1.id
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y awscli
+
+              while true; do
+                private_ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+                timestamp=$(date +"%Y%m%d%H%M%S")
+                echo $private_ip > /tmp/private_ip_$timestamp.txt
+                aws s3 cp /tmp/private_ip_$timestamp.txt s3://sm-bucket-evaluacion/outputs/private_ip_$timestamp.txt
+                sleep 300
+              done
+              EOF
+
+  tags = {
+    Name     = "evalucion_sm"
+    username = "smejia"
+  }
+
+  key_name              = aws_key_pair.my_key_pair.key_name
+  security_groups       = [aws_security_group.my_security_group.id]
+  associate_public_ip_address = true
+  iam_instance_profile  = aws_iam_instance_profile.ec2_instance_profile.name
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name     = "route-tables-sm"
+    username = "smejia"
+  }
+}
+
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_cloudwatch_log_group" "my_log_group" {
+  name              = "/aws/ec2/terraform-example"
+  retention_in_days = 14
+
+  tags = {
+    username = "smejia"
+  }
+}
+
+resource "aws_cloudwatch_log_stream" "my_log_stream" {
+  name           = "example-log-stream"
+  log_group_name = aws_cloudwatch_log_group.my_log_group.name
+
+}
